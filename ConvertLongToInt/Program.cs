3672073ca8b34580bd349257241966d3a907efe0
taukir4u <taukir4u@gmail.com>
2014-12-26 19:43:44 +0600

    C#
    Signed-off-by: taukir4u <taukir4u@gmail.com>

---
+++ ConvertLongToInt/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConvertLongToInt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long number;
+            int printNumber;
+
+            number = Convert.ToInt64(Console.ReadLine());
+
+            printNumber = (Int16) number;
+
+            Console.WriteLine(printNumber);
+
+            Console.ReadKey();
+
+        }
+    }
+}
